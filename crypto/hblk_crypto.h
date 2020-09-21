a5b1cd0709bb9af8ce8218b26f373778dde52636
Thomas Graeff <676@holbertonschool.com>
2020-09-20 18:04:46 -0700

    Initial add of ec_create

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -8,7 +8,10 @@
 
 #include <openssl/opensslconf.h>
 #include <openssl/ec.h>
+#include <openssl/obj_mac.h>
 #include <openssl/sha.h>
+
+#define EC_CURVE NID_secp256k1
 
 uint8_t *sha256(int8_t const *s, size_t len,
 	uint8_t digest[SHA256_DIGEST_LENGTH]);
