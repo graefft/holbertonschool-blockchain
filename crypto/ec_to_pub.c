6bd69e5b713ac659086706509b1eccffe3ec2d55
Thomas Graeff <676@holbertonschool.com>
2020-09-22 23:22:20 -0700

    Initial commit of EC key to pub

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -10,5 +10,18 @@
  */
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
-	return (NULL);
+	const EC_GROUP *group;
+
+	const EC_POINT *point;
+
+	if (!key || !pub)
+		return (NULL);
+
+	group = EC_KEY_get0_group(key);
+	point = EC_KEY_get0_public_key(key);
+	if (!EC_POINT_point2oct(group, point,
+		POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL))
+		return (NULL);
+
+	return (pub);
 }
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -13,7 +13,11 @@
 
 #define EC_CURVE NID_secp256k1
 
+#define EC_PUB_LEN 65 /* constant 04 prefix, 32-byte x, 32-byte y */
+
 uint8_t *sha256(int8_t const *s, size_t len,
 	uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
+
 #endif  /* __HBLK_CRYPTO__ */
