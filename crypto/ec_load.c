26fb0136a7d50ab339dfaaffc4c1d6fb3a76d51f
Thomas Graeff <676@holbertonschool.com>
2020-10-08 01:03:07 -0700

    Initial commit of ec_load

---
+++ crypto/ec_load.c
@@ -0,0 +1,45 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_load - loads an existing EC key pair on the disk
+ *
+ * @folder: path to the folder from which to load the keys
+ *
+ * Return: pointer to EC key pair, or NULL on failure
+ */
+EC_KEY *ec_load(char const *folder)
+{
+	char buffer[BUFSIZ];
+	EC_KEY *key = NULL;
+	FILE *fp;
+
+	if (!key || !folder)
+		return (NULL);
+
+	sprintf(buffer, "%s/%s",  folder, PRI_FILENAME);
+	fp = fopen(buffer, "r");
+	if (!fp)
+		return (NULL);
+
+	/* Write Private key to PEM */
+	if (!PEM_read_ECPrivateKey(fp, &key, NULL, NULL))
+	{
+		fclose(fp);
+		return (NULL);
+	}
+	fclose(fp);
+
+	sprintf(buffer, "%s/%s", folder, PUB_FILENAME);
+	fp = fopen(buffer, "r");
+	if (!fp)
+		return (NULL);
+
+	/* Write Public key to PEM */
+	if (!PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
+	{
+		fclose(fp);
+		return (NULL);
+	}
+	fclose(fp);
+	return (key);
+}
