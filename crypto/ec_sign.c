aa302f2bbca236f88538d3ce0ed7ab8a74b9f431
Thomas Graeff <676@holbertonschool.com>
2020-10-12 10:36:02 -0700

    Initial commit of ec_sign

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -18,11 +18,11 @@ uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 	if (!key || !msg || !sig)
 		return (NULL);
 
+	sig->len = (uint8_t)ECDSA_size(key);
+
 	if (!SHA256(msg, msglen, digest))
 		return (NULL);
-	sig->len = (uint8_t)ECDSA_size(key);
-	if (!sig->len)
-		return (NULL);
+	
 	if (ECDSA_sign(EC_CURVE, digest, SHA256_DIGEST_LENGTH, sig->sig,
 		 (unsigned int *)&(sig->len), (EC_KEY *)key))
 		return (NULL);
