cc14ba2e6867b4687bb639a7df6d035ba63db5ff
Thomas Graeff <676@holbertonschool.com>
2020-10-13 00:19:22 -0700

    Initial commit of block_create

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -18,7 +18,22 @@ block_t *block_create(block_t const *prev, int8_t const *data,
 	uint32_t data_len)
 {
 	block_t *new_block;
+	uint32_t length;
 
-	new_block = malloc(sizeof(*new_block));
+	new_block = calloc(1, sizeof(*new_block));
+	if (!new_block)
+	{
+		perror("Calloc");
+		return (NULL);
+	}
+	new_block->info.index = prev->info.index + 1;
+	new_block->info.difficulty = 0;
+	new_block->info.nonce = 0;
+	new_block->info.timestamp = (uint64_t)time(NULL);
+	memcpy(new_block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
+	length = data_len < BLOCKCHAIN_DATA_MAX ? data_len : BLOCKCHAIN_DATA_MAX;
+	memcpy(new_block->data.buffer, data, length);
+	new_block->data.len = length;
 
-	new_block->info = 
+	return (new_block);
+}
