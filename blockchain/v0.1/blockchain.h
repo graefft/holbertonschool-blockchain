654459bc0fd003d2bda963f09bd42b5041899a0a
Thomas Graeff <676@holbertonschool.com>
2020-10-27 03:43:22 -0700

    Moves GENESIS definitions to header for use with new functions

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -16,6 +16,12 @@
 #include "../../crypto/hblk_crypto.h"
 
 #define BLOCKCHAIN_DATA_MAX 1024
+
+#define GENESIS_DATA        "Holberton School"
+#define GENESIS_DATA_LEN    16
+#define GENESIS_HASH         "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03" 
+#define GENESIS_TIMESTAMP   1537578000
+
 #define HBLK_MAGIC "HBLK"
 #define HBLK_VERSION "0.1"
 
--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -1,10 +1,5 @@
 #include "blockchain.h"
 
-#define GENESIS_DATA		"Holberton School"
-#define GENESIS_DATA_LEN	16
-#define GENESIS_HASH		"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
-#define GENESIS_TIMESTAMP	1537578000
-
 /**
  * genesis_create - creates Genesis Block
  *
