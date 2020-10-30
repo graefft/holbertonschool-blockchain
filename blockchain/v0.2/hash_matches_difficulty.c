346aed876c620940c29733e14c82a9ce5bd1da43
Thomas Graeff <676@holbertonschool.com>
2020-10-29 23:59:37 -0700

    Initial commit hash_matches_difficulty

---
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -0,0 +1,13 @@
+#include "blockchain.h"
+
+/**
+ * hash_matches_difficulty - checks whether a given hash matches 
+ *                           a given difficulty
+ * @hash: hash to check
+ * @difficulty: minimum difficulty the hash should match
+ * Return: 1 if difficulty is respected or 0 otherwise
+ */
+int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH])
+{
+	return (0);
+}
