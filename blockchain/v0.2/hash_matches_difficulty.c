b19e05be0c2c9c545cc08b8bb9b192e1abb09488
Thomas Graeff <676@holbertonschool.com>
2020-11-03 02:21:49 -0800

    Checks if hash matches difficulty

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -1,29 +1,29 @@
 #include "blockchain.h"
 
 /**
- * hash_matches_difficulty - checks whether a given hash matches 
+ * hash_matches_difficulty - checks whether a given hash matches
  *                           a given difficulty
  * @hash: hash to check
  * @difficulty: minimum difficulty the hash should match
  * Return: 1 if difficulty is respected or 0 otherwise
  */
-int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH], uint32_t difficulty)
+int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
+	uint32_t difficulty)
 {
-	uint8_t *p;
+	uint8_t i = 0, msb = difficulty & 7;
 
-	while (p < hash + SHA256_DIGEST_LENGTH)
+	difficulty -= difficulty & 7;
+	difficulty /= 8;
+
+	while (i < difficulty)
 	{
-		for (i = 8; i >= 1; i--)
-		{
-			if ((*p >> 1) & 1)
-				return (difficulty);
-			difficulty++;
-		}
-		p++;
+		if (hash[i] & 1)
+			break;
+		i++;
 	}
-	if (hash[difficulty] >> (8 - (difficulty & 7)))
-		return (0);
-	return (1);
 
-	return (0);
+	if (hash[difficulty] >> (8 - msb))
+		return (0);
+
+	return (1);
 }
