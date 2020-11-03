e1f62252a317f2746b731c98ffad6e4c7ff39e4f
Thomas Graeff <676@holbertonschool.com>
2020-11-03 00:00:01 -0800

    Initial

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -7,7 +7,23 @@
  * @difficulty: minimum difficulty the hash should match
  * Return: 1 if difficulty is respected or 0 otherwise
  */
-int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH])
+int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH], uint32_t difficulty)
 {
+	uint8_t *p;
+
+	while (p < hash + SHA256_DIGEST_LENGTH)
+	{
+		for (i = 8; i >= 1; i--)
+		{
+			if ((*p >> 1) & 1)
+				return (difficulty);
+			difficulty++;
+		}
+		p++;
+	}
+	if (hash[difficulty] >> (8 - (difficulty & 7)))
+		return (0);
+	return (1);
+
 	return (0);
 }
